c23d637573e9eda56d0466ee510fd74fb84a1775
Nadina <sheriefnadina@gmail.com>
2023-11-22 13:41:04 +0100

    feat/ es-2

--- es-2.js
+++ es-2.js
@@ -1,9 +1,15 @@
-/* Scrivi una funzione JavaScript che:
-1. Accetta un array di stringhe come argomento.
-2. Calcola la concatenazione di tutte le stringhe che iniziano con la lettera "A" o "a".
-(10 punti)
-3. Se la lunghezza della stringa concatenata supera i 10 caratteri, troncala a 10
-caratteri e restituiscila. (5 punti)
-4. Se il ciclo si completa senza che la stringa superi i 10 caratteri, restituisci la
-stringa concatenata.
- */
+// Esercizio 2: Cicli e Condizioni
+
+const concatenaStringhe = (arrayDiStringhe) => {
+    let stringaConcatenata = '';
+    for (let i = 0; i < arrayDiStringhe.length; i++) {
+        let stringa = arrayDiStringhe[i];
+        if (stringa.toLowerCase().startsWith('a')) {
+            stringaConcatenata += stringa;
+        };
+        if (stringaConcatenata.length > 10) {
+            stringaConcatenata = stringaConcatenata.slice(0, 10);
+        };
+    };
+    return stringaConcatenata;
+};
