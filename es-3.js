ffba858e7af0e30b41702905df7c1098dbff0bf1
Nadina <sheriefnadina@gmail.com>
2023-11-22 11:10:49 +0100

    feat/ es-3

--- es-3.js
+++ es-3.js
@@ -1,15 +1,23 @@
-/* 1. Aggiungi una nuova proprietà rating all'oggetto libro, che rappresenta il rating
-del libro, e inizializzalo a 6 .
-2. Aggiungi un metodo increaseRating che accetta come argomento un valore
-numerico e che incrementa il rating del libro del valore passato alla funzione. (10
-punti)
-Il nuovo rating risultante, non può superare 10. (2.5 punti)
-3. Rimuovi la proprietà year dall'oggetto libro. (2.5 punti)
-4. Stampa in console ogni proprietà dell'oggetto libro in questo formato: "[chiave]:
-[valore]" . (5 punti) */
+// Esercizio 3: Array e Oggetti 
 
 const book = {
     title: "Il Grande Gatsby",
     author: "F. Scott Fitzgerald",
     year: 1925,
 };
+
+book.rating = 6;
+book.increaseRating = function (numero){
+    let nuovoRating = this.rating += numero;
+    nuovoRating > 10 ? book.rating = 10 : nuovoRating;
+};
+
+delete book.year;
+
+const chiavi = Object.keys(book);
+for (let i = 0; i < chiavi.length; i++) {
+    const chiave = chiavi[i];
+    const valore = book[chiave];
+    console.log(`${chiave}: ${valore}`);
+};
+
