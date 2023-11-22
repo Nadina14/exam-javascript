cb8b70872a82d41e4fadf065d4bd6c261edbe2f3
Nadina <sheriefnadina@gmail.com>
2023-11-22 12:25:32 +0100

    feat/ es-5

--- es-5.js
+++ es-5.js
@@ -1,13 +1,4 @@
-/* Scrivi una funzione power per elevare un numero ad una potenza data:
-1. Prende due numeri come parametri ( base e exponent ).
-2. Controlla che effettivamente siano stati passati due numeri. In caso contrario,
-lancia un errore che mostra nel messaggio quale dei parametri non è un
-numero. Se entrambi non sono dei numeri, il messaggio d’errore li mostra
-entrambi. (15 punti)
-3. Nel caso in cui sia avvenuto un errore, la funzione ritorna null , altrimenti ritorna
-base elevato alla exponent . (2.5 punti)
-4. Indipendentemente dal successo o dal fallimento, stampa in console un
-messaggio che indica che l'operazione è completa. (2.5 punti) */
+// Esercizio 5: Error Handling e Parsing
 
 const elevaNumeri = (base, esponente) => {
     if (isNaN(base) && isNaN(esponente)) {
@@ -21,7 +12,7 @@ const elevaNumeri = (base, esponente) => {
     };
 };
 try {
-    elevaNumeri(2, 2);
+    elevaNumeri();
 } catch (error) {
     console.error(error);
 } finally {
