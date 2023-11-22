0e091a51cd8c2923d6b3b3ae7d046853bd751b99
Nadina <sheriefnadina@gmail.com>
2023-11-22 13:27:05 +0100

    feat/ es-5

--- es-5.js
+++ es-5.js
@@ -1,20 +1,22 @@
 // Esercizio 5: Error Handling e Parsing
 
 const elevaNumeri = (base, esponente) => {
-    if (isNaN(base) && isNaN(esponente)) {
-        throw new Error(`${base} e ${esponente} non sono dei numeri`);
-    } else if (isNaN(esponente)) {
-        throw new Error(`${esponente} non é un numero`);
-    } else if (isNaN(base)) {
-        throw new Error(`${base} non é un numero`);
-    }else{
-        return base ** esponente;
-    };
+    try {
+        if (isNaN(base) && isNaN(esponente)) {
+            throw new Error(`${base} e ${esponente} non sono dei numeri`);
+        } else if (isNaN(esponente)) {
+            throw new Error(`${esponente} non é un numero`);
+        } else if (isNaN(base)) {
+            throw new Error(`${base} non é un numero`);
+        }else{
+            return base ** esponente;
+        };
+    } catch (error) {
+        console.error(error);
+        return null;
+    } finally {
+        console.log(`l'operazione é completata`);
+    }
 };
-try {
-    elevaNumeri();
-} catch (error) {
-    console.error(error);
-} finally {
-    console.log(`l'operazione é completata`);
-}
+
+elevaNumeri();
