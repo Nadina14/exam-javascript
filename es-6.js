3af80742bb3fd341c4ba1192095ae6611ddf7ac1
Nadina <sheriefnadina@gmail.com>
2023-11-22 13:20:48 +0100

    feat/ es-6

--- es-6.js
+++ es-6.js
@@ -1,30 +1,36 @@
-/* 1. In un commento, scrivi quanti oggetti l’espressione soprastante ha inserito in
-memoria ed elencali. (2.5 punti)
-2. Crea una variabile chiamata teamLead che faccia riferimento alla proprietà lead
-dell'oggetto devTeam . Aggiungi "GraphQL" alle skills di teamLead .
-3. Crea una copia dell'oggetto devTeam e memorizzala in una nuova variabile
-chiamata devTeamCopy . Non puoi usare la funzione integrata structuredClone . Poi,
-cambia la proprietà projectName di devTeam2 in "MobileApp". (2.5 punti)
-4. Costruisci una funzione che crea e restituisce un oggetto tester a partire dal
-parametro name (stringa). Durante la creazione dell’oggetto, la funzione
-dovrebbe anche assegnare un array skills con tre stringhe casuali tra
-"JavaScript", "React", "Node.js", "HTML", "CSS", "Python", "Django",
-"PostgreSQL". (5 punti)
-Usa questa funzione per aggiungere un nuovo tester a devTeamCopy . (2.5 punti)
-5. In un commento, scrivi quanti oggetti sono stati creati in memoria oltre a quelli
-iniziali, e giustifica la tua risposta. (2.5 punti) */
+// Esercizio 6: Clonazione di Oggetti
 
-
-
+// Nell'espressione sottostante vi sono 8 oggetti: 'devTeam', 'lead', al cui interno é presente 'skills'(oggetto di tipo lista, ovvero l'array) e 'testers'(altro array, ovvero oggetto di tipo lista), che a sua volta contiene 2 oggetti contententi altri 2 array 'skills'.
 
 const devTeam = {
     lead: {
-    name: "Alice",
-    skills: ["JavaScript", "React", "Node.js"]
+        name: "Alice",
+        skills: ["JavaScript", "React", "Node.js"]
     },
     testers: [
-    { name: "Bob", skills: ["HTML", "CSS", "JavaScript"] },
-    { name: "Charlie", skills: ["Python", "Django", "PostgreSQL"] }
+        { name: "Bob", skills: ["HTML", "CSS", "JavaScript"] },
+        { name: "Charlie", skills: ["Python", "Django", "PostgreSQL"] }
     ],
     projectName: "WebApp"
-   };
+};
+
+const teamLead = devTeam.lead;
+teamLead.skills += "GraphQL";
+const devTeamCopy = JSON.parse(JSON.stringify(devTeam));
+devTeamCopy.projectName = "MobileApp";
+
+const creaTester = (name) => {
+    const tester = {name};
+    tester.skills = [];
+    const randomSkills = ["JavaScript", "React", "Node.js", "HTML", "CSS", "Python", "Django", "PostgreSQL"];
+    for (let i = 0; i < 3; i++) {
+        const randomSkill = Math.floor(Math.random() * randomSkills.length);
+        tester.skills.push(randomSkills[randomSkill]);
+    }
+    return tester;
+};
+
+const nuovoTester = creaTester("Gianni");
+devTeamCopy.testers.push(nuovoTester);
+
+// In memoria sono stati aggiunti 2 oggetti, un 'tester' che al suo interno presenta un altro oggetto di tipo lista 'skills'
